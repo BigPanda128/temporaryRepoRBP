9d62f2de192c950d01a7b34c6c09ca5e161ddcc6
rpostma <rpostma@localhost>
2016-02-19 14:05:40 -0500

    initial commit with a basic hello loop

---
+++ temporaryRepoRBP/src/edu/saintjoe/cs/rpostma/tempDriver.java
@@ -0,0 +1,14 @@
+package edu.saintjoe.cs.rpostma;
+
+public class tempDriver {
+
+	public static void main(String[] args) {
+		// print Hello world 100 times
+		for (int i = 1; i <= 100; ++i) {
+			
+			System.out.println("Hello poor peasant number: " + i);
+		}
+
+	}
+
+}
