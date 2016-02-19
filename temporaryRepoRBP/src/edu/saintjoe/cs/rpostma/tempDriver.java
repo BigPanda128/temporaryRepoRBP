ba08ecff8bc6a02f1ccacf92170c1cb213e61e2c
rpostma <rpostma@localhost>
2016-02-19 14:19:50 -0500

    changed meat of code form hello world basic to a powers loop with an array

--- temporaryRepoRBP/src/edu/saintjoe/cs/rpostma/tempDriver.java
+++ temporaryRepoRBP/src/edu/saintjoe/cs/rpostma/tempDriver.java
@@ -3,11 +3,16 @@ package edu.saintjoe.cs.rpostma;
 public class tempDriver {
 
 	public static void main(String[] args) {
-		// print hello code 100 times
-		for (int i = 1; i <= 100; ++i) {
-			System.out.println("Hello poor peasant number: " + i);
-	
+		int array[];
+		int size = 10;//array size
+		
+		array = new int[size];
+		
+		for (int i = 0; i < size; ++i){
+			array[i] = i*i;
+			System.out.println("Array1 has " + array[i] + " in slot " + i);
 		}
+		System.out.println("Notice a pattern?");
 	
 	}
 	
