7d5a2a62f71623d72880cd002087ff64517bf881
rpostma <rpostma@localhost>
2016-02-19 14:08:07 -0500

    messed with spacing of code

--- temporaryRepoRBP/src/edu/saintjoe/cs/rpostma/tempDriver.java
+++ temporaryRepoRBP/src/edu/saintjoe/cs/rpostma/tempDriver.java
@@ -3,12 +3,12 @@ package edu.saintjoe.cs.rpostma;
 public class tempDriver {
 
 	public static void main(String[] args) {
-		// print Hello world 100 times
+		// print hello code 100 times
 		for (int i = 1; i <= 100; ++i) {
-			
 			System.out.println("Hello poor peasant number: " + i);
+	
 		}
-
+	
 	}
-
+	
 }
